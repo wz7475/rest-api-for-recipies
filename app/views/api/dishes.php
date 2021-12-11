faf5f04d5b8d48695e95ff7f505fb3410b969383
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2021-12-11 09:02:48 +0100

    fix api

--- app/views/api/dishes.php
+++ app/views/api/dishes.php
@@ -1,11 +1,5 @@
 
 
 <?php foreach ($data['dishes'] as $dish) : ?>
-<<<<<<< HEAD
-
-    <?php echo $dish->name; ?>
-
-=======
     <?php echo $dish->name . "<br>"; ?>
->>>>>>> wojtek
 <?php endforeach; ?>
