be4a8ae74d4f487f2a0a3519dfce8993e7d996b4
IMatynia <45515142+IMatynia@users.noreply.github.com>
2021-12-13 15:46:19 +0100

    implemented recommendations v0.0.0.0.1

--- app/models/Api.php
+++ app/models/Api.php
@@ -21,8 +21,7 @@ class Api
     {
         $results = $this->findAlldishesRaw();
         $output = [];
-        foreach($results as $dish)
-        {
+        foreach ($results as $dish) {
             array_push($output, $this->fillDishWithData($dish));
         }
 
@@ -62,18 +61,12 @@ class Api
         return $this->db->execute();
     }
 
-    public function getRecommendations($user_id, $amount)
+    public function getRankedTags($user_id)
     {
-        //get all recipies that are not disliked
-        $this->db->query('
-        SELECT * FROM dishes
-        WHERE id not in (
-            SELECT dish_id FROM dislikes 
-            WHERE user_id = :user_id
-            )
-        ');
-        $this->db->bind(':user_id', $user_id);
-        $available_dishes = $this->db->resultSet();
+        /*
+        IF a tag is in disliked dishes -> negative score
+        IF a tag is in used dishes -> positive score
+        */
 
         //Get all disliked tags
         $this->db->query('
@@ -86,59 +79,128 @@ class Api
         $this->db->bind(':user_id', $user_id);
         $disliked_tags = $this->db->resultSet();
 
+        //Get all used tags (ergo liked tags)
+        $this->db->query('
+        SELECT tags_id FROM dishes
+        WHERE id in (
+            SELECT dish_id FROM used
+            WHERE user_id = :user_id
+            )
+        ');
+        $this->db->bind(':user_id', $user_id);
+        $used_tags = $this->db->resultSet();
+
         //count them
-        $bucket = [];
+        $bucket_disliked = [];
         foreach ($disliked_tags as $tags) {
             $tags = explode(",", $tags->tags_id);
             foreach ($tags as $tag) {
-                if (isset($bucket[$tag])) {
-                    $bucket[$tag]++;
+                if (isset($bucket_disliked[$tag])) {
+                    $bucket_disliked[$tag]++;
                 } else {
-                    $bucket[$tag] = 1;
+                    $bucket_disliked[$tag] = 1;
+                }
+            }
+        }
+        $bucket_used = [];
+        foreach ($used_tags as $tags) {
+            $tags = explode(",", $tags->tags_id);
+            foreach ($tags as $tag) {
+                if (isset($bucket_used[$tag])) {
+                    $bucket_used[$tag]++;
+                } else {
+                    $bucket_used[$tag] = 1;
                 }
             }
         }
 
         //normalize
-        $worst_tag_occurences = max($bucket);
+        if(count($bucket_disliked)) $worst_tag_occurences = max($bucket_disliked);
+        if(count($bucket_used)) $best_tag_occurences = max($bucket_used);
         $ranked_tags = [];
-        foreach(array_keys($bucket) as $tag) 
-        {
-            $ranked_tags[$tag] = -($bucket[$tag]/$worst_tag_occurences);
+
+        foreach (array_keys($bucket_disliked) as $tag) {
+            $ranked_tags[$tag] = - ($bucket_disliked[$tag] / $worst_tag_occurences);
         }
 
-        //calculate each dishes score
-        foreach($available_dishes as $dish)
+        foreach (array_keys($bucket_used) as $tag) {
+            if (isset($ranked_tags[$tag])) {
+            } else {
+                $ranked_tags[$tag] = ($bucket_used[$tag] / $best_tag_occurences);
+            }
+        }
+        return $ranked_tags;
+    }
+
+    public function getUsageRank($user_id)
+    {
+        $this->db->query('
+        SELECT dish_id FROM used 
+        WHERE user_id = :user_id
+        ');
+        $this->db->bind(':user_id', $user_id);
+        $used_dishes = $this->db->resultSet();
+
+        $rank_by_usage = [];
+        foreach($used_dishes as $dish)
         {
+            //PÓŹNIEJ BĘDZIE TO FUNKCJA OD CZASU
+            $rank_by_usage[$dish->dish_id] = -1;
+        }
+        return $rank_by_usage;
+    }
+
+    public function getRecommendations($amount, $user_id)
+    {
+        //get all recipies that are not disliked
+        $this->db->query('
+        SELECT * FROM dishes
+        WHERE id not in (
+            SELECT dish_id FROM dislikes 
+            WHERE user_id = :user_id
+            )
+        ');
+        $this->db->bind(':user_id', $user_id);
+        $available_dishes = $this->db->resultSet();
+
+        $rank_by_usage = $this->getUsageRank($user_id);
+        $ranked_tags = $this->getRankedTags($user_id);
+
+        //calculate each dishes score
+        foreach ($available_dishes as $dish) {
             $tags = explode(",", $dish->tags_id);
             $score = 0;
 
             $tag_score = 0;
-            foreach($tags as $tag)
-            {
-                $tag_score+=$ranked_tags[$tag];
+            foreach ($tags as $tag) {
+                if (isset($ranked_tags[$tag])) {
+                    $tag_score += $ranked_tags[$tag];
+                }
             }
             $score += ($tag_score / count($tags));
             
-            $dish["score"] = $score;
+            if(isset($rank_by_usage[$dish->id]))
+            {
+                $score += $rank_by_usage[$dish->id];
+            }
+
+            $dish->score = $score;
         }
 
-    }
+        function sortByScore($a, $b)
+        {
+            if ($a->score < $b->score) {
+                return 1;
+            } else if ($a->score == $b->score) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
 
-    public function sortByScore($a, $b)
-    {
-        if($a["score"]>$b["score"])
-        {
-            return 1;
-        }
-        else if($a["score"]==$b["score"])
-        {
-            return 0;
-        }
-        else
-        {
-            return -1;
-        }
+        //sort dishes by score
+        usort($available_dishes, "sortByScore");
+        return array_slice($available_dishes, 0, min($amount, count($available_dishes)));
     }
 
     public function findTagByID($tag_id)
@@ -150,7 +212,7 @@ class Api
         return  $this->db->single();
     }
 
-    
+
     public function getTags($tag_id_string)
     {
         $tags = explode(",", $tag_id_string);
@@ -163,15 +225,15 @@ class Api
 
     public function fillDishWithData($dish)
     {
-         //Adding tags from tag ids
-         $dish->tags = $this->getTags($dish->tags_id);
-         unset($dish->tags_id);
- 
-         //Adding recipie from recipie_id
-         $recipie = $this->getRecipieByID($dish->recipie_id)->steps;
-         unset($dish->recipie_id);
-         $dish->recipie = utf8_encode($recipie);
+        //Adding tags from tag ids
+        $dish->tags = $this->getTags($dish->tags_id);
+        unset($dish->tags_id);
 
-         return $dish;
+        //Adding recipie from recipie_id
+        $recipie = $this->getRecipieByID($dish->recipie_id)->steps;
+        unset($dish->recipie_id);
+        $dish->recipie = utf8_encode($recipie);
+
+        return $dish;
     }
 }
