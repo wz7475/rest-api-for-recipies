874f7cef6c829aebcd4dbd1e4dce31994312dfc8
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-01-11 21:34:27 +0100

    presonalized config

--- app/config/config.php
+++ app/config/config.php
@@ -1,21 +1,31 @@
 <?php
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
-//Database params
-define('DB_HOST', $cleardb_server); //Add your db host
-define('DB_USER', $cleardb_username); // Add your DB root
-define('DB_PASS', $cleardb_password); //Add your DB pass
-define('DB_NAME', $cleardb_db); //Add your DB Name
-// define('DB_HOST', 'localhost'); //Add your db host
-// define('DB_USER', 'root'); // Add your DB root
-// define('DB_PASS', ''); //Add your DB pass
-// define('DB_NAME', 'myblog'); //Add your DB Name
 //APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
-//URLROOT (Dynamic links)
-define('URLROOT', 'https://granny-crud.herokuapp.com');
+
+if(APPROOT == "D:\Programming\gr9k\granny-grud\app")
+{
+    define('DB_HOST', 'eu-cdbr-west-02.cleardb.net'); //Add your db host
+    define('DB_USER', 'bf6d0ebec5a2f9'); // Add your DB root
+    define('DB_PASS', '1c957d41'); //Add your DB pass
+    define('DB_NAME', 'heroku_5c1b88cec025cd7'); //Add your DB Name
+
+    define('URLROOT', 'http://localhost/');
+}
+else
+{
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"], 1);
+    
+    // Database params
+    define('DB_HOST', $cleardb_server); //Add your db host
+    define('DB_USER', $cleardb_username); // Add your DB root
+    define('DB_PASS', $cleardb_password); //Add your DB pass
+    define('DB_NAME', $cleardb_db); //Add your DB Name
+
+    define('URLROOT', 'https://granny-crud.herokuapp.com');
+}
 //Sitename
 define('SITENAME', 'Login & Register script');
